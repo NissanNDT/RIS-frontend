25ad31ef4b3f47d70c2f078a7703c58a5f612fc2
Medina Rodriguez <nissandefectndt@gmail.com>
2026-06-09 12:36:38 -0600

    Cmbio en login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -82,7 +82,7 @@ const Login = () => {
 
         <form onSubmit={handleSubmit} className="login-form">
           <label>
-            Correo
+            Correo -debe ir en minusculas-
             <input
               type="email"
               name="email"
